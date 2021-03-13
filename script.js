f35d5522ec8bab4ac913da3e0fb2742351590bd7
victor kitesh <victorkitesh@gmail.com>
2021-03-13 22:19:34 +0300

    added liters per day

---
+++ script.js
@@ -0,0 +1,37 @@
+function liters(shedA){
+
+    document.writeln("Your production in Shed A "+ shedA +" liters per day");
+    document.write("<hr>");
+    
+};
+
+ function secondshed (shedB){
+    document.writeln("Your production in Shed B "+ shedB +" liters per day");
+    document.write("<hr>");
+};
+function thirdshed (shedC){
+    document.write("Your production in Shed C "+ shedC +" liters per day");
+    document.write("<hr>");
+};
+function forthshed (shedD){
+    document.write("Your production in Shed D "+ shedD +" liters per day");
+    document.write("<hr>");
+};
+function allshed (total){
+    document.write("The total production is " + total +" litres per day")
+    document.write("<hr>");
+}
+liters(510)
+secondshed(308)
+thirdshed(486)
+forthshed(572)
+allshed(510+308+486+572)
+
+
+var header= document.getElementById('header')
+header.style.color="blue"
+
+
+
+
+
